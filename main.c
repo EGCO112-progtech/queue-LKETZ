8c5671d8ccbdb918fb193aa4a72207116304c7ed
LKETZ <159238584+LKETZ@users.noreply.github.com>
2024-02-23 07:34:18 +0000

    new learn

--- main.c
+++ main.c
@@ -7,25 +7,30 @@
 
 int main(int argc , char **argv) {
   NodePtr headPtr=NULL;
-   NodePtr tailPtr=NULL;
-/* For struct Queue
-  Queue  q;
-   q. headPtr=NULL;
-   q.tailPtr=NULL;
-   q.size=0;
-*/
-   int i,x;
-   
+  NodePtr tailPtr=NULL;
+// 5 x 9 x 7 x 8 2 x
+  int x;
+  enqueue(&headPtr, &tailPtr,5);
+  enqueue(&headPtr, &tailPtr,6);
+  enqueue(&headPtr, &tailPtr,7);
+  x = dequeue(&headPtr, &tailPtr);
+  printf("%d",x);
 
- for(i=1;i<argc;i++){
-        if(strcmp(argv[i],"x")==0){
-            x=dequeue(&headPtr,&tailPtr);
-            printf("dequeing %d\n",x);
-        }
-        else {
-       enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
+  x = dequeue(&headPtr, &tailPtr);
+  printf("%d",x);
+
+  x = dequeue(&headPtr, &tailPtr);
+  printf("%d",x);
+
+//  for(i=1;i<argc;i++){
+//         if(strcmp(argv[i],"x")==0){
+//             x=dequeue(&headPtr,&tailPtr);
+//             printf("dequeing %d\n",x);
+//         }
+//         else {
+//        enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
            
-        }
- }
+//         }
+//  }
   return 0;
 }
