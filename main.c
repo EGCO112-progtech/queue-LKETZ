dc186068360954e1ec975e0043eaeef4ec733023
LKETZ <159238584+LKETZ@users.noreply.github.com>
2024-02-23 07:52:53 +0000

    complete2

--- main.c
+++ main.c
@@ -22,15 +22,15 @@ int main(int argc , char **argv) {
   x = dequeue(&headPtr, &tailPtr);
   printf("%d",x);
 
-//  for(i=1;i<argc;i++){
-//         if(strcmp(argv[i],"x")==0){
-//             x=dequeue(&headPtr,&tailPtr);
-//             printf("dequeing %d\n",x);
-//         }
-//         else {
-//        enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
+ for(i=1;i<argc;i++){
+        if(strcmp(argv[i],"x")==0){
+            x=dequeue(&headPtr,&tailPtr);
+            printf("dequeing %d\n",x);
+        }
+        else {
+       enqueue_struct(&headPtr,&tailPtr, atoi(argv[i]));
            
-//         }
-//  }
+        }
+ }
   return 0;
 }
